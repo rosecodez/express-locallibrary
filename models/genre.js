d1590143e84cde1ff20a79cb01da90cdc0b87fa2
Rose <rosebeats09@gmail.com>
2024-05-05 15:53:39 -0400

    Add work

---
+++ models/genre.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose');
+
+const { Schema } = mongoose;
+const GenreSchema = new Schema({
+  name: {
+    type: String, required: true, minLength: 3, maxLength: 100,
+  },
+});
+// Virtual for genre's URL
+GenreSchema.virtual('url').get(function () {
+  return `/catalog/genre/${this._id}`;
+});
+module.exports = mongoose.model('Genre', GenreSchema);
