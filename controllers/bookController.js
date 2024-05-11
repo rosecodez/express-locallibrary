b35dbe49f4fcfa3df528f8331b33168663fada04
Rose <rosebeats09@gmail.com>
2024-05-11 00:14:51 -0400

    Add book instance detail page

--- controllers/bookController.js
+++ controllers/bookController.js
@@ -1,6 +1,10 @@
 const asyncHandler = require('express-async-handler');
 const Book = require('../models/book');
 
+const BookInstance = require('../models/author');
+const Author = require('../models/author');
+const Genre = require('../models/genre');
+
 exports.index = asyncHandler(async (req, res, next) => {
   // Get details of books, book instances, authors and genre counts (in parallel)
   const [
@@ -39,7 +43,24 @@ exports.book_list = asyncHandler(async (req, res, next) => {
 
 // Display detail page for a specific book.
 exports.book_detail = asyncHandler(async (req, res, next) => {
-  res.send(`NOT IMPLEMENTED: Book detail: ${req.params.id}`);
+  // Get details of books, book instances for specific book
+  const [book, bookInstances] = await Promise.all([
+    Book.findById(req.params.id).populate('author').populate('genre').exec(),
+    BookInstance.find({ book: req.params.id }).exec(),
+  ]);
+
+  if (book === null) {
+    // No results.
+    const err = new Error('Book not found');
+    err.status = 404;
+    return next(err);
+  }
+
+  res.render('book_detail', {
+    title: book.title,
+    book,
+    book_instances: bookInstances,
+  });
 });
 
 // Display book create form on GET.
