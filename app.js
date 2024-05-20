830578b25a983b8b601c5c4d3db669bb3198aa05
Rose <rosebeats09@gmail.com>
2024-05-20 15:47:03 -0400

    Add rate limiting to the API routes

--- app.js
+++ app.js
@@ -6,12 +6,22 @@ const logger = require('morgan');
 
 const compression = require('compression');
 const helmet = require('helmet');
+const RateLimit = require('express-rate-limit');
 const mongoose = require('mongoose');
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
 const catalogRouter = require('./routes/catalog'); // Import routes for "catalog" area of site
 
 const app = express();
+
+// Set up rate limiter: maximum of twenty requests per minute
+const limiter = RateLimit({
+  windowMs: 1 * 60 * 1000, // 1 minute
+  max: 20,
+});
+
+// Apply rate limiter to all requests
+app.use(limiter);
 
 // Add helmet to the middleware chain.
 // Set CSP headers to allow our Bootstrap and Jquery to be served
